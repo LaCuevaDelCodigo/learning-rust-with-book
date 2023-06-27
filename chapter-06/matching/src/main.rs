dc09fc6dc228daac6ea8e9ade16f96307b4e5608
LaCuevaDelCodig <cuevadelcodigo@gmail.com>
2023-06-27 19:04:00 +0200

    chapter 6 part 2

---
+++ chapter-06/matching/src/main.rs
@@ -0,0 +1,70 @@
+fn main() {
+    {
+        #[derive(Debug)]
+        enum UsState {
+            Alabama,
+            Alaska,
+        }
+
+        enum Coin {
+            Penny,
+            Nickel,
+            Dime,
+            Quarter(UsState),
+        }
+
+        fn value_in_cents(coin: Coin) -> u8 {
+            match coin {
+                Coin::Penny => 1,
+                Coin::Nickel => 5,
+                Coin::Dime => 10,
+                Coin::Quarter(state) => {
+                    println!("State quarter from {:?}!", state);
+                    25
+                }
+            }
+        }
+
+        println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)))
+    }
+
+    {
+        fn plus_one(x: Option<i32>) -> Option<i32> {
+            match x {
+                None => None,
+                Some(i) => Some(i + 1),
+            }
+        }
+
+        let five = Some(5);
+        let six = plus_one(five);
+        let none = plus_one(None);
+
+        println!("{}", six.unwrap())
+    }
+
+    {
+        let dice_roll = 9;
+        match dice_roll {
+            3 => add_fancy_hat(),
+            7 => remove_fancy_hat(),
+            other => move_player(other),
+        }
+
+        fn add_fancy_hat() {}
+        fn remove_fancy_hat() {}
+        fn move_player(num_spaces: u8) {}
+    }
+
+    {
+        let dice_roll = 9;
+        match dice_roll {
+            3 => add_fancy_hat(),
+            7 => remove_fancy_hat(),
+            _ => (),
+        }
+
+        fn add_fancy_hat() {}
+        fn remove_fancy_hat() {}
+    }
+}
